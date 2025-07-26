4bf34536284fddd6f4917d7f93369f27f8752c7c
Denis Zalivin <shaixulud@gmail.com>
2025-07-25 23:16:32 -0400

    Rewrite queries comparison

--- django_sql_test/diff_utils.py
+++ django_sql_test/diff_utils.py
@@ -1,4 +1,6 @@
 import difflib
+from dataclasses import dataclass
+from enum import Enum
 
 from sql_metadata.generalizator import Generalizator
 
@@ -14,12 +16,63 @@ new_color = DIFF_NEW_COLOR or green_color
 default_color = DIFF_DEFAULT_COLOR or reset_color
 
 
+class DiffType(Enum):
+    ADDED = "+"
+    REMOVED = "-"
+    UNCHANGED = " "
+
+
+@dataclass
+class DiffLine:
+    diff_type: DiffType
+    original_query: str
+    generalized_query: str
+
+
 def get_raw_queries(captured_queries: list[dict]) -> list[str]:
     return [query["sql"] for query in captured_queries]
 
 
 def generalize_queries(captured_queries: list[str]) -> list[str]:
     return [Generalizator(query).generalize for query in captured_queries]
+
+
+def build_queries_diff_list(new_captured_queries: list[dict], old_captured_queries: list[dict]) -> list[DiffLine]:
+    new_queries = generalize_queries(get_raw_queries(new_captured_queries))
+    old_queries = generalize_queries(get_raw_queries(old_captured_queries))
+
+    new_query_idx = 0
+    old_query_idx = 0
+    queries_diff_list = []
+
+    for diff_line in difflib.ndiff(old_queries, new_queries):
+        if diff_line.startswith("?"):
+            continue
+
+        if diff_line.startswith("-"):
+            original_query = old_captured_queries[old_query_idx]["sql"]
+            original_query = f"- {original_query}"
+            queries_diff_list.append(
+                DiffLine(diff_type=DiffType.REMOVED, original_query=original_query, generalized_query=diff_line)
+            )
+            old_query_idx += 1
+        elif diff_line.startswith("+"):
+            original_query = new_captured_queries[new_query_idx]["sql"]
+            original_query = f"+ {original_query}"
+            queries_diff_list.append(
+                DiffLine(diff_type=DiffType.ADDED, original_query=original_query, generalized_query=diff_line)
+            )
+            new_query_idx += 1
+        else:
+            original_query = new_captured_queries[new_query_idx]["sql"]
+            original_query = f"  {original_query}"
+            queries_diff_list.append(
+                DiffLine(diff_type=DiffType.UNCHANGED, original_query=original_query, generalized_query=diff_line)
+            )
+            new_query_idx += 1
+            old_query_idx += 1
+
+    return queries_diff_list
 
 
 def create_queries_diff(
@@ -29,13 +82,10 @@ def create_queries_diff(
     generalized_diff: bool,
 ) -> tuple[str, bool]:
     is_same = True
-    new_queries = generalize_queries(get_raw_queries(new_captured_queries))
-    old_queries = generalize_queries(get_raw_queries(old_captured_queries))
+    queries_diff_list = build_queries_diff_list(new_captured_queries, old_captured_queries)
 
-    if not old_queries:
-        generalized_diff_list = new_queries
-    else:
-        generalized_diff_list = (i for i in difflib.ndiff(old_queries, new_queries) if not i.startswith("?"))
+    old_queries = 1
+    generalized_diff_list = queries_diff_list
 
     if generalized_diff:
         if diff_only and old_queries:
---
+++ tests/test_build_queries_diff_list.py
@@ -0,0 +1,7 @@
+import unittest
+
+from django_sql_test.diff_utils import build_queries_diff_list
+
+
+class BuildQueriesDiffListTestCase(unittest.TestCase):
+    pass
